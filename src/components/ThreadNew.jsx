28512b97761f2c8c26dcf22780374fa0ca97fa08
ittyi <vioiv.k4@gmail.com>
2024-03-21 02:38:22 +0900

    ✨ add: 先に top への画面遷移機能を完成

--- src/components/ThreadNew.jsx
+++ src/components/ThreadNew.jsx
@@ -1,7 +1,23 @@
+import { Link } from "react-router-dom";
+
 export default function ThreadNew() {
+//   function search(formData) {
+//     console.log("in", formData)
+//     // const query = formData.get("query");
+//     // alert(`You searched for '${query}'`);
+//   }
+
   return (
-    <div>
-      <h1>ThreadNew</h1>
-    </div>
+    <>
+      {/* <h1>スレッド新規作成</h1> */}
+      {/* <form action="post">
+        <button>test</button>
+      </form> */}
+      <form method="get" action="http://localhost:3000/">
+        <input name="query" />
+        <button type="submit">Search</button>
+      </form>
+      <Link to={`/`}>top へ戻る</Link>
+    </>
   );
-};
+}
