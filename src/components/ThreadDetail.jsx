ae38c0639be36f36548bb64f032528f56f11b17f
ittyi <vioiv.k4@gmail.com>
2024-03-24 14:05:12 +0900

    ✨ add: スレッド内メッセージ投稿機能を追加

--- src/components/ThreadDetail.jsx
+++ src/components/ThreadDetail.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useLocation } from "react-router-dom";
+import axios from "axios";
 
 export default function ThreadDetail() {
-  const threadTitle = useLocation();
+  const useloc = useLocation();
   const { threadId } = useParams();
   const [listMessage, setListMessage] = useState([{}]);
 
@@ -18,15 +19,37 @@ export default function ThreadDetail() {
       });
   }, [threadId]);
 
+
+  const [post, setPost] = useState("");
+  function createThreadMessage() {
+    const url =
+      "https://railway.bulletinboard.techtrain.dev/threads/" +
+      threadId +
+      "/posts";
+
+    axios
+      .post(url, {
+        post: post,
+      })
+      .then((response) => {
+        console.log(response.data);
+        window.location.reload()
+      });
+  }
+
   return (
     <>
-      <h2>{threadTitle.state}</h2>
+      <h2>{useloc.state}</h2>
       <main>
         <ul>
           {listMessage.map((message) => (
             <li key={String(message.id)}>{message.post}</li>
           ))}
         </ul>
+        <div>
+          <input onChange={(event) => setPost(event.target.value)}/>
+          <button onClick={createThreadMessage}>投稿</button>
+        </div>
       </main>
     </>
   );
